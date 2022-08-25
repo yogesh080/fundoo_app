25c368d18ab68172a0daa1056473071e786eb0e8
Yogesh <yogeshbietjhs@gmail.com>
2022-08-25 12:37:24 +0530

    note deleted

--- Fundoo_backend/BusinessLayer/Interface/INotesBL.cs
+++ Fundoo_backend/BusinessLayer/Interface/INotesBL.cs
@@ -11,6 +11,8 @@ namespace BusinessLayer.Interface
         public NotesEntity AddNotes(NoteCreateModel noteCreateModel, long userId);
         public IEnumerable<NotesEntity> ReadNotes(long userId);
         public NotesEntity UpdateNote(NoteCreateModel noteModel, long NoteId, long userId);
+        public bool DeleteNotes(long userId, long noteId);
+
 
 
     }
