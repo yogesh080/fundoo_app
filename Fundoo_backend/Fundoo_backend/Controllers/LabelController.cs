d64cc86764ac293d08705e573cdd31fd4aac8d93
Yogesh <yogeshbietjhs@gmail.com>
2022-09-01 11:18:27 +0530

    Label redis implementation done

--- Fundoo_backend/Fundoo_backend/Controllers/LabelController.cs
+++ Fundoo_backend/Fundoo_backend/Controllers/LabelController.cs
@@ -9,6 +9,14 @@ using RepositoryLayer.Entity;
 using RepositoryLayer.Context;
 using RepositoryLayer.Service;
 using RepositoryLayer.Interface;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Fundoo_backend.Controllers
 {
@@ -17,12 +25,18 @@ namespace Fundoo_backend.Controllers
     public class LabelController : ControllerBase
     {
         private readonly ILabelBL labelBL;
+        private readonly IMemoryCache memoryCache;
+        private readonly FundooContext context;
+        private readonly IDistributedCache distributedCache;
 
-        public LabelController(ILabelBL labelBL)
+        public LabelController(ILabelBL labelBL, IMemoryCache memoryCache, FundooContext context, IDistributedCache distributedCache)
         {
             this.labelBL = labelBL;
+            this.memoryCache = memoryCache;
+            this.context = context;
+            this.distributedCache = distributedCache;
         }
-
+        [Authorize]
         [HttpPost]
         [Route("Create")]
         public ActionResult AddLabel(long notesId, string labelname)
@@ -52,8 +66,8 @@ namespace Fundoo_backend.Controllers
 
         }
 
-        
 
+        [Authorize]
         [HttpDelete]
         [Route("Delete")]
         public ActionResult DeleteLabel(long labelId)
@@ -73,7 +87,7 @@ namespace Fundoo_backend.Controllers
 
         }
 
-
+        [Authorize]
         [HttpGet]
         [Route("Read")]
         public IActionResult ReadLabel(long labelId)
@@ -121,6 +135,31 @@ namespace Fundoo_backend.Controllers
             {
                 throw;
             }
+        }
+
+        [HttpGet("redis")]
+        public async Task<IActionResult> GetAllNotesUsingRedisCache()
+        {
+            var cacheKey = "LabelList";
+            string serializedLabelList;
+            var LabelList = new List<NoteLabel>();
+            var redisLabelList = await distributedCache.GetAsync(cacheKey);
+            if (redisLabelList != null)
+            {
+                serializedLabelList = Encoding.UTF8.GetString(redisLabelList);
+                LabelList = JsonConvert.DeserializeObject<List<NoteLabel>>(serializedLabelList);
+            }
+            else
+            {
+                LabelList = await context.LabelTable.ToListAsync();
+                serializedLabelList = JsonConvert.SerializeObject(LabelList);
+                redisLabelList = Encoding.UTF8.GetBytes(serializedLabelList);
+                var options = new DistributedCacheEntryOptions()
+                    .SetAbsoluteExpiration(DateTime.Now.AddMinutes(10))
+                    .SetSlidingExpiration(TimeSpan.FromMinutes(2));
+                await distributedCache.SetAsync(cacheKey, redisLabelList, options);
+            }
+            return Ok(LabelList);
         }
     }
 }
